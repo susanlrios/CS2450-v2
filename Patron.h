523f2e41cf47443a13b7a108842dc2cb7d4bc4e4
cmansfield <chrismansfield3@hotmail.com>
2016-03-30 00:24:34 -0600

    Patron v0.2

---
+++ Patron.h
@@ -0,0 +1,336 @@
+#ifndef PAT_H
+#define PAT_H
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <time.h>
+#include <regex>
+
+
+class Patron
+{
+private:
+	const std::string DATE_PATTERN = R"(^[1-9][0-2]{0,1}/[0-3]{0,1}[0-9]/(?:19|20)[0-9]{2}$)";
+	const std::string MONEY_PATTERN = R"(^\d{1,8}(\.{1}\d+){0,1}$)";
+	const std::string ADDRESS_PATTERN = R"([a-zA-Z0-9_ ]{3,50}$)";
+	const std::string NAME_PATTERN = R"(^[A-Z][a-zA-Z ]{3,20}$)";
+	const std::string ID_PATTERN = R"(^[0-9]+$)";
+
+	std::string _firstName;
+	std::string _LastName;
+	std::string _address;
+	std::string _birthDate;
+	std::string _joinDate;
+	int _outstandingFees;
+	size_t _ID;
+
+	const std::string CurrentDateTime();
+	void SetFirstName( std::string );
+	void SetLastName( std::string );
+	void SetBirthdate( std::string );
+	void SetJoinDate( std::string );
+	void SetID( std::string );
+	std::vector<std::string> Split( char*, char );
+
+public:
+	Patron();
+	Patron( std::string, std::string, std::string, std::string, std::string, std::string );
+	~Patron();
+	void SetAddress( std::string );
+	void SetFees( std::string );
+	const std::string& GetFirstName();
+	const std::string& GetLastName();
+	const std::string& GetAddress();
+	const std::string& GetBirthDate();
+	const std::string& GetJoinDate();
+	int GetFees();
+	size_t GetID();
+	bool IsMinor();
+	void Write( std::ostream& );
+	void Read( std::istream& );
+};
+
+
+
+Patron::Patron()
+{
+	_firstName = "John";
+	_LastName = "Doe";
+	_address = "";
+	_birthDate = CurrentDateTime();
+	_joinDate = CurrentDateTime();
+	_outstandingFees = 0;
+	_ID = 0;
+}
+
+
+Patron::Patron( std::string firstName, std::string lastName, std::string address, std::string birthDate, std::string joinDate, std::string ID )
+{
+	SetFirstName( firstName );
+
+	SetLastName( lastName );
+
+	SetAddress( address );
+
+	SetBirthdate( birthDate );
+
+	SetJoinDate( joinDate );
+
+	SetID( ID );
+
+	_outstandingFees = 0;
+}
+
+
+Patron::~Patron()
+{
+}
+
+
+void Patron::SetFirstName( std::string firstName )
+{
+	if ( std::regex_match( firstName, std::regex( NAME_PATTERN ) ) )
+	{
+		_firstName = firstName;
+	}
+	else
+	{
+		throw std::logic_error( "Bad argument for first name" );
+	}
+}
+
+
+void Patron::SetLastName( std::string lastName )
+{
+	if ( std::regex_match( lastName, std::regex( NAME_PATTERN ) ) )
+	{
+		_LastName = lastName;
+	}
+	else
+	{
+		throw std::logic_error( "Bad argument for last name" );
+	}
+}
+
+
+void Patron::SetAddress( std::string address )
+{
+	if ( std::regex_match( address, std::regex( ADDRESS_PATTERN ) ) )
+	{
+		_address = address;
+	}
+	else
+	{
+		throw std::logic_error( "Bad argument for address" );
+	}
+}
+
+
+void Patron::SetBirthdate( std::string birthDate )
+{
+	if ( std::regex_match( birthDate, std::regex( DATE_PATTERN ) ) )
+	{
+		_birthDate = birthDate;
+	}
+	else
+	{
+		throw std::logic_error( "Bad argument for birth date" );
+	}
+}
+
+
+void Patron::SetJoinDate( std::string joinDate )
+{
+	if ( std::regex_match( joinDate, std::regex( DATE_PATTERN ) ) )
+	{
+		_joinDate = joinDate;
+	}
+	else
+	{
+		throw std::logic_error( "Bad argument for join date" );
+	}
+}
+
+
+void Patron::SetFees( std::string fees )
+{
+	if ( std::regex_match( fees, std::regex( MONEY_PATTERN ) ) )
+	{
+		_outstandingFees = stod( fees );
+	}
+	else
+	{
+		throw std::logic_error( "Bad argument for outstanding fees" );
+	}
+}
+
+
+void Patron::SetID( std::string ID )
+{
+	if ( std::regex_match( ID, std::regex( ID_PATTERN ) ) )
+	{
+		_ID = stoi( ID );
+	}
+	else
+	{
+		throw std::logic_error( "Bad argument for ID" );
+	}
+}
+
+
+const std::string Patron::CurrentDateTime() {
+	time_t     now = time( 0 );
+	struct tm  tstruct;
+	char       buffer[80];
+	tstruct = *localtime( &now );
+	strftime( buffer, sizeof( buffer ), "%m/%d/%Y", &tstruct );
+
+	return buffer;
+}
+
+
+const std::string& Patron::GetFirstName()
+{
+	return _firstName;
+}
+
+
+const std::string& Patron::GetLastName()
+{
+	return _LastName;
+}
+
+
+const std::string& Patron::GetAddress()
+{
+	return _address;
+}
+
+
+const std::string& Patron::GetBirthDate()
+{
+	return _birthDate;
+}
+
+
+const std::string& Patron::GetJoinDate()
+{
+	return _joinDate;
+}
+
+
+int Patron::GetFees()
+{
+	return _outstandingFees;
+}
+
+
+size_t Patron::GetID()
+{
+	return _ID;
+}
+
+
+bool Patron::IsMinor()
+{
+	std::vector<std::string> splitBirth;
+	std::vector<std::string> splitNow;
+	std::string now = CurrentDateTime();
+
+	splitBirth = Split( &_birthDate[0], '/' );
+	splitNow = Split( &now[0], '/' );
+
+	try
+	{
+		if ( stoi( splitBirth[2] ) > stoi( splitNow[2] ) - 18 ) 
+		{ 
+			return true; 
+		}
+		else if ( stoi( splitBirth[2] ) == stoi( splitNow[2] ) - 18 )
+		{
+			if ( stoi( splitBirth[0] ) < stoi( splitNow[0] ) ) 
+			{ 
+				return true; 
+			}
+			else if ( stoi( splitBirth[0] ) == stoi( splitNow[0] ) )
+			{
+				if ( stoi( splitBirth[1] ) < stoi( splitNow[1] ) ) { return true; }
+			}
+		}
+	}
+	catch ( const std::exception& )
+	{
+		throw std::logic_error( "Could not check if the patron was a minor" );
+	}
+
+	return false;
+}
+
+
+void Patron::Write( std::ostream& out )
+{
+	out << 
+		_firstName << "," << 
+		_LastName << "," << 
+		_address << "," << 
+		_birthDate << "," << 
+		_joinDate << "," << 
+		_outstandingFees << "," << 
+		_ID << std::endl;
+}
+
+
+void Patron::Read( std::istream& in )
+{
+	std::vector<std::string> splitVector;
+	std::string lineInput;
+	
+	getline( in, lineInput );
+
+	if ( lineInput != "" )
+	{
+		splitVector = Split( &lineInput[0], ',' );
+	}
+
+	try
+	{
+		SetFirstName( splitVector[0] );
+		SetLastName( splitVector[1] );
+		SetAddress( splitVector[2] );
+		SetBirthdate( splitVector[3] );
+		SetJoinDate( splitVector[4] );
+		SetFees( splitVector[5] );
+		SetID( splitVector[6] );
+	}
+	catch ( const std::exception& error )
+	{
+		//std::string errorMessage = "Could not read patron from the file: " + error.what();
+		throw std::logic_error( error.what() );
+	}
+}
+
+
+std::vector<std::string> Patron::Split( char* input, char delim = ' ' )
+{
+	std::vector<std::string> splitResult;
+
+	do {
+		char *begin = input;
+
+		while ( *input != delim && *input )
+		{
+			input++;
+		}
+
+		splitResult.push_back( std::string( begin, input ) );
+
+	} while ( *input++ != 0 );
+
+	return splitResult;
+}
+
+
+
+#endif PAT_H
---
+++ Patron_Test_Driver.cpp
@@ -0,0 +1,108 @@
+
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include "Patron.h"
+
+
+int main(int argc, char* argv[])
+{
+	std::vector<Patron*> patrons;
+	std::string fileName = "output.csv";
+	std::ostringstream convert;
+	std::string input;
+	std::ofstream fileOut;
+	std::ifstream fileIn;
+
+
+	// used for testing file creation
+	try
+	{
+		for ( size_t i = 0; i < 10; i++ )
+		{
+			convert.str( "" );
+			convert.clear();
+			convert << i;
+			patrons.push_back( new Patron( "Chris", "Mansfield", "1234 Street Name_Orem_UT", "7/8/1989", "3/26/2016", convert.str() ) );
+		}
+	}
+	catch ( const std::exception& err )
+	{
+		std::cout << err.what() << std::endl;
+	}
+
+	// used for file input
+	//try
+	//{
+	//	for ( size_t i = 0; i < 10; i++ )
+	//	{
+	//		patrons.push_back( new Patron() );
+	//	}
+	//}
+	//catch ( const std::exception& err )
+	//{
+	//	std::cout << err.what() << std::endl;
+	//}
+
+	fileOut.open( fileName );
+
+	if ( !fileOut.is_open() )
+	{
+		std::cout << "Error: could not create file" << fileName << std::endl;
+	}
+	else
+	{
+		fileOut << "First Name,Last Name,Address,Birth Date,Join Date,Outstanding Balance,ID" << std::endl;
+
+		for each ( Patron* patron in patrons )
+		{
+			patron->Write( fileOut );
+		}
+	}
+
+	fileOut.close();
+
+	fileIn.open( fileName );
+
+	if ( !fileIn.is_open() )
+	{
+		std::cout << "Error: could not open file" << fileName << std::endl;
+	}
+	else
+	{
+		getline( fileIn, input );
+
+		for each ( Patron* patron in patrons )
+		{
+			patron->Read( fileIn );
+		}
+	}
+	
+	fileIn.close();
+
+	std::cout << patrons[0]->GetFirstName() << " " << patrons[0]->GetLastName() << std::endl <<
+		patrons[0]->GetAddress() << std::endl <<
+		"Patron ID: " << patrons[0]->GetID() << std::endl <<
+		"Join Date: " << patrons[0]->GetJoinDate() << std::endl <<
+		"Outstanding Fees Due: $" << patrons[0]->GetFees() << std::endl;
+
+	if ( patrons[0]->IsMinor() )
+	{
+		std::cout << "They are a Minor" << std::endl;
+	}
+
+	for each ( Patron* patron in patrons )
+	{
+		delete( patron );
+	}
+
+	std::cin.clear();
+	do {
+		getline( std::cin, input );
+	} while ( input != "" );
+
+	return 0;
+}
