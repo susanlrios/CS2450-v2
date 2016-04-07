31553826830ec05169d7e81829847b1e92ac31c8
cmansfield <chrismansfield3@hotmail.com>
2016-04-07 11:10:13 -0600

    main v0.4

--- main.cpp
+++ main.cpp
@@ -1,14 +1,13 @@
-// Main v0.3
+// Main v0.4
 
 // *************************
 // Todo List
 
-// 1. Decided if I want one search function but pass it function pointers
+// 1. Decided if the search function will be passed function pointers
 // 3. Make it so the user can create a new patron
-// 4. Make it so the program will save the databases at exit
-// 5. Change the book class's check out function to include a patron pointer
-// 7. Change book's ISBN type to string
-// 8. Decide if the patron pointers in the hash map
+// 4. Make it so the program will save the book database at exit
+// 5. Change the book class's check out function to include a void pointer
+// 8. Decide if the only patron pointers reside in the hash map
 // *************************
 
 // *************************
@@ -16,7 +15,6 @@
 
 // 4. Fix the issue with losing database data when running into a save error
 // 5. Fix the clear sreen function so it doesn't use a system call
-// 6. Fix the enum / switch statement in main
 // *************************
 
 
@@ -29,6 +27,24 @@
 
 #include "Library.h"
 
+enum OptionMain { 
+	checkIN = 1, 
+	checkOUT = 2, 
+	listBooks = 3, 
+	listOverdue = 4, 
+	listByPatron = 5, 
+	changeDate = 6, 
+	addDay = 7, 
+	exitProgram = 8 
+};
+
+enum OptionSearch {
+	searchByBook = 1,
+	searchByPatron = 2,
+	exitToMain = 3
+};
+
+
 
 void ClearScreen()
 {
@@ -37,10 +53,47 @@ void ClearScreen()
 }
 
 
+const std::string SearchMenu( Library& library )
+{
+	std::string input = "";
+
+	do
+	{
+		std::cout << " Search by patron or by book?" << std::endl <<
+			"1-Search by book" << std::endl <<
+			"2-Search by Patron" << std::endl <<
+			"3-Return to main menu" << std::endl;
+
+		getline( std::cin, input );
+
+		switch ( stoi( input ) )
+		{
+		case searchByBook:
+			// Display all books
+			// Get user input for the desired book
+			// Return books ISBN
+
+			break;
+		case searchByPatron:
+			// Display all patrons
+			library.displayPatrons( std::cout );
+
+			// Get user input for the desired patron
+			std::cout << "testing";
+
+			// Return the Patron's ID
+
+			break;
+		}
+	} while ( input != std::to_string( exitToMain ) );
+
+	return "-1";
+}
+
+
 
 int main( int argc, const char * argv[] ) 
 {
-	enum Option { checkIN = 1, checkOUT, listBooks, listOverdue, listByPatron, changeDate, exit };
 	Library* library = new Library();
 	std::string patronFileName = "patronDB.csv";
 	std::string bookFileName = "bookDB.csv";
@@ -104,28 +157,34 @@ int main( int argc, const char * argv[] )
 	}
 
     do {
-        std::cout << "1-Check in book" << std::endl;
-        std::cout << "2-Check out book" << std::endl;
-        std::cout << "3-List all books" << std::endl;
-        std::cout << "4-List all overdue books" << std::endl;
-        std::cout << "5-List all books checked out by a patron" << std::endl;
-        std::cout << "6-Change current date" << std::endl;
-		std::cout << "7-Exit" << std::endl << std::endl;
-		std::cout << "What would you like to do? ";
+		std::cout << "Current Date: " << Date::GetCurrentDate() << std::endl << std::endl <<
+			"1-Check in book" << std::endl <<
+			"2-Check out book" << std::endl <<
+			"3-List all books" << std::endl <<
+			"4-List all overdue books" << std::endl <<
+			"5-List all books checked out by a patron" << std::endl <<
+			"6-Change current date" << std::endl <<
+			"7-Advance the date by one day" << std::endl <<
+			"8-Exit" << std::endl << std::endl <<
+			"What would you like to do? ";
+
 		getline( std::cin, input );
 
 		try
 		{
 			switch ( stoi( input ) )
 			{
-			case 1:				// Check in a book
+			case checkIN:					// Check in a book
+				std::cout << "Checking in a book.";
+				SearchMenu( *library );
+
 				// search book
 				// see if it's actually checked out
 				// check in
 				// remove book from patron's unsorted map
 
 				break;
-			case 2:				// Check out a book
+			case checkOUT:					// Check out a book
 				// list patrons
 				// get user input for patron
 				// search patrons
@@ -137,22 +196,22 @@ int main( int argc, const char * argv[] )
 				library->checkout( "0", "98298347" );
 
 				break;
-			case 3:				// List all books
+			case listBooks:					// List all books
 				// pass std::cout ref to function to display all books
 
 				break;
-			case 4:				// List all overdue books
+			case listOverdue:				// List all overdue books
 				// pass std::cout ref to function
 				// preform a LINQ query on all overdue books
 				// display overdue books
 
 				break;
-			case 5:				// List all books checked out by patron
+			case listByPatron:				// List all books checked out by patron
 				// search patron
 				// pass std::cout ref to function to display books checked by patron
 
 				break;
-			case 6:				// Change the current date
+			case changeDate:				// Change the current date
 				std::cout << "\nPlease enter the current date" << std::endl <<
 					"example: 02/04/2016 or 2/4/2016" << std::endl;
 
@@ -173,6 +232,10 @@ int main( int argc, const char * argv[] )
 				} while ( input == "-1" );
 
 				break;
+
+			case addDay:				// Add one day to the current date
+				Date::AddDayToCurrent();
+				break;
 			}
 		}
 		catch ( const std::exception& error )
@@ -184,7 +247,7 @@ int main( int argc, const char * argv[] )
 
 		ClearScreen();
 
-    } while ( input != "7" );
+    } while ( input != std::to_string( exitProgram ) );
 
 
 	// Save the databases to their files
